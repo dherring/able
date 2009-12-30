dfe0ed33226b3bfdbf83b82f8bc16b6b4c5b2cff
D Herring <dherring@at.tentpost.dot.com>
2009-12-30 01:21:06 -0500

    more package browser thoughts
    this is ballooning into an inspector...

--- pb.lisp
+++ pb.lisp
@@ -7,21 +7,24 @@
   (with-ltk ()
     (let* ((top (make-instance 'frame))
            (tree (make-instance 'treeview
-                                :master top))
+                                :master top
+                                :columns 2
+                                ))
            (sc (make-instance 'scrollbar :master top)))
+
+      (format-wish "~a heading #~a -~(~a~) ~a" (widget-path tree) 0 :text "name")
+      (format-wish "~a heading #~a -~(~a~) ~a" (widget-path tree) 1 :text "#syms")
+      
       (dolist (p (sort (mapcar #'package-name (list-all-packages))
                        #'string<))
         (make-instance 'ltk::treeitem
                        :tree tree
                        :text p
+                       ;; need to pass -values {name #syms}...
                        ))
       (configure tree "yscrollcommand" (format nil "~A set" (widget-path sc)))
       (configure sc "command" (format nil "~A yview" (widget-path tree)))
 
-      ;;(grid top 0 0 :sticky :nwse)
-      ;;(pack top :fill :both :expand t)
-      ;;(grid tree 0 0)
-      ;;(grid sc 0 1 :sticky :ns)
       (pack top :side :left :fill :both :expand t)
       (pack tree :side :left :fill :both :expand t)
       (pack sc :side :left :fill :y :expand nil)
@@ -31,3 +34,44 @@
                          (let ((item (treeview-focus tree)))
                            (unless (string= item "")
                              (format t "package: ~A~%" (ltk::treeview-item tree item :text)))))))))
+
+(defun comma-sep-string (list)
+  (format nil "~{~A~#[~:;, ~]~}" list))
+(defun sorted-package-names (package-list)
+  (comma-sep-string
+   (sort (mapcar #'package-name package-list)
+         #'string<)))
+(defun show-package (name)
+  #| TODO: add buttons to manipulate the package
+  intern, import, export, rename, delete, use-package, etc.
+  Basically everything in CLHS 11.2.
+  |#
+  (with-ltk ()
+    (let* ((package (find-package name))
+           (top (make-instance 'frame))
+           (row 0))
+      ;;(wm-title *tk* "show-package")
+      (pack top :fill :both :expand t)
+      (flet ((show (name text)
+               (let ((tag (make-instance 'label
+                                         :master top
+                                         :text name))
+                     (value (make-instance 'label
+                                           :master top
+                                           :text text)))
+                 (grid tag row 0 :sticky :w)
+                 (grid value row 1 :sticky :ew)
+                 (incf row)
+                 ;;(pack tag)
+                 ;;(pack value)
+                 )))
+        (show "name" (or (package-name package)
+                         "<no-name>"))
+        (show "nicknames" (comma-sep-string (package-nicknames package)))
+        (show "shadows" (comma-sep-string
+                         (sort (mapcar #'symbol-name
+                                       (package-shadowing-symbols package))
+                               #'string<)))
+        (show "uses" (sorted-package-names (package-use-list package)))
+        (show "used by" (sorted-package-names (package-used-by-list package)))
+        ))))
